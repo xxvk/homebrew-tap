6a4fbd66977784da310ba61e3b9bf4a1015087ba
Van <xxvk@outlook.com>
2026-07-16 21:51:45 +0900

    Fix macos-data formula version

--- Formula/macos-data.rb
+++ Formula/macos-data.rb
@@ -3,6 +3,7 @@ class MacosData < Formula
   homepage "https://github.com/xxvk/macos-data-cli"
   url "https://github.com/xxvk/macos-data-cli/releases/download/v0.1.1/macos-data-0.1.1-macos-arm64.tar.gz"
   sha256 "3c04869519547b221944433f6221c0cf5cb584420b3700f37f014ed9751e05db"
+  version "0.1.1"
   license "MIT"
 
   def install
