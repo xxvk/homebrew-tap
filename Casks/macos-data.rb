f2b625cb6dc4d1170c0c453ebf4f9900f693bb99
Van <xxvk@outlook.com>
2026-08-14 23:23:30 +0900

    Update macos-data to 0.7.1

--- Casks/macos-data.rb
+++ Casks/macos-data.rb
@@ -1,6 +1,6 @@
 cask "macos-data" do
-  version "0.7.0"
-  sha256 "4c104a6ddcd588c934174134c118af5cb3b7d7a67eb30b5d5eca597023580ce1"
+  version "0.7.1"
+  sha256 "a73eaf5eb40ab9aeed29f0bda85375472a615cf0126a4898281f1f578e62adbd"
 
   url "https://github.com/xxvk/macos-data-cli/releases/download/v#{version}/macos-data-#{version}-macos-arm64.tar.gz"
   name "macos-data"
